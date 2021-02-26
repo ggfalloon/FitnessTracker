73653075cbe18dcc910a95368bc96b2632a16684
Gabriele Falloon <ggfalloon@gmail.com>
2021-02-26 11:55:20 -0600

    corrected route error

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const Transaction = require("../models/workout.js");
+const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", ({ body }, res) => {
-    Transaction.create(body)
-        .then(dbTransaction => {
-            res.json(dbTransaction);
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
@@ -13,8 +13,8 @@ router.get("/api/workouts", ({ body }, res) => {
 
 router.post("/api/transaction/bulk", ({ body }, res) => {
     Transaction.insertMany(body)
-        .then(dbTransaction => {
-            res.json(dbTransaction);
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
@@ -22,10 +22,10 @@ router.post("/api/transaction/bulk", ({ body }, res) => {
 });
 
 router.get("/api/transaction", (req, res) => {
-    Transaction.find({})
+    Workout.find({})
         .sort({ date: -1 })
-        .then(dbTransaction => {
-            res.json(dbTransaction);
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
--- routes/html.js
+++ routes/html.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const path = require("path");
 // Each of the below routes just handles the HTML page that the user gets sent to.
 
 // index route loads view.html
@@ -12,7 +13,7 @@ router.get("/exercise", function (req, res) {
 });
 
 // // stats route
-app.get("/stats", function (req, res) {
+router.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
