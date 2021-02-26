eeb82d5741a86d0268120f567f69f2b1d971bf8f
Gabriele Falloon <ggfalloon@gmail.com>
2021-02-26 13:42:25 -0600

    corrected model date type

--- models/workout.js
+++ models/workout.js
@@ -3,19 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter a name for transaction"
-    },
-    value: {
-        type: Number,
-        required: "Enter an amount"
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+    day: { type: Date, default: Date.now },
+
+    exercises: [
+        {
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number
+        }
+    ]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
