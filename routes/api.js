a0d964eda3da53395fb50f3747c4f502d1efc320
Gabriele Falloon <ggfalloon@gmail.com>
2021-02-26 11:55:39 -0600

    functioning html routes

--- routes/api.js
+++ routes/api.js
@@ -11,25 +11,25 @@ router.get("/api/workouts", ({ body }, res) => {
         });
 });
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-    Transaction.insertMany(body)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
+// router.post("/api/transaction/bulk", ({ body }, res) => {
+//     Transaction.insertMany(body)
+//         .then(dbWorkout => {
+//             res.json(dbWorkout);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
 
-router.get("/api/transaction", (req, res) => {
-    Workout.find({})
-        .sort({ date: -1 })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
+// router.get("/api/transaction", (req, res) => {
+//     Workout.find({})
+//         .sort({ date: -1 })
+//         .then(dbWorkout => {
+//             res.json(dbWorkout);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
 
 module.exports = router;
