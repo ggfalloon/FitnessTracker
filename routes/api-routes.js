92795b468b8e7e934a3268afaf1c3fe86c8a17ce
Gabriele Falloon <ggfalloon@gmail.com>
2021-03-01 10:12:17 -0600

    renamed routes js

---
+++ routes/api-routes.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/exercise", ({ body }, res) => {
+    Workout.insertMany(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// router.post("/api/workouts", ({ body }, res) => {
+//     Workout.insertMany(body)
+//         .then(dbWorkout => {
+//             res.json(dbWorkout);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
+
+// router.get("/api/workouts", ({ body }, res) => {
+//     Workout.create(body)
+//         .then(dbWorkout => {
+//             res.json(dbWorkout);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
+
+// router.get("/api/exercise/:id", (req, res) => {
+//     Workout.find({})
+//         .sort({ date: -1 })
+//         .then(dbWorkout => {
+//             res.json(dbWorkout);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// });
+
+// router.get("api/workouts/range", (req, res) => {
+//     Workout.find({})
+//         .then(dbWorkout => {
+//             res.json(dbWorkout);
+//         })
+//         .catch(err => {
+//             res.status(400).json(err);
+//         });
+// })
+
+module.exports = router;
